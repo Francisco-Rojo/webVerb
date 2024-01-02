1592614b31f7950e6c95a3ac2dcc98bd6561d40f
Francisco Rojo <francisco_2710_javier@hotmail.com>
2024-01-01 20:57:50 -0600

    updated index - fuction fetch for read file json

--- js/index.js
+++ js/index.js
@@ -1,559 +1,4 @@
-const list = [
-    {
-        "verb": "be",
-        "past": "was / were",
-        "participle": "been",
-        "traslade": "ser / estar"
-    },
-    {
-        "verb": "beat",
-        "past": "beat",
-        "participle": "beaten",
-        "traslade": "golpear"
-    },
-    {
-        "verb": "become",
-        "past": "became",
-        "participle": "become",
-        "traslade": "llegar a ser"
-    },
-    {
-        "verb": "begin",
-        "past": "began",
-        "participle": "begun",
-        "traslade": "empezar"
-    },
-    {
-        "verb": "bend",
-        "past": "bent",
-        "participle": "bent",
-        "traslade": "doblar"
-    },
-    {
-        "verb": "bet",
-        "past": "bet",
-        "participle": "bet",
-        "traslade": "apostar"
-    },
-    {
-        "verb": "bite",
-        "past": "bit",
-        "participle": "bitten",
-        "traslade": "morder"
-    },
-    {
-        "verb": "bleed",
-        "past": "bled",
-        "participle": "bled",
-        "traslade": "sangrar"
-    },
-    {
-        "verb": "blow",
-        "past": "blew",
-        "participle": "blown",
-        "traslade": "soplar"
-    },
-    {
-        "verb": "break",
-        "past": "broke",
-        "participle": "broken",
-        "traslade": "romper"
-    },
-    {
-        "verb": "bring",
-        "past": "brought",
-        "participle": "brought",
-        "traslade": "traer"
-    },
-    {
-        "verb": "build",
-        "past": "built",
-        "participle": "built",
-        "traslade": "construir"
-    },
-    {
-        "verb": "burst",
-        "past": "burst",
-        "participle": "burst",
-        "traslade": "estallar"
-    },
-    {
-        "verb": "buy",
-        "past": "bought",
-        "participle": "bought",
-        "traslade": "comprar"
-    },
-    {
-        "verb": "catch",
-        "past": "caught",
-        "participle": "caught",
-        "traslade": "atrapar"
-    },
-    {
-        "verb": "come",
-        "past": "came",
-        "participle": "come",
-        "traslade": "venir"
-    },
-    {
-        "verb": "choose",
-        "past": "chose",
-        "participle": "chose",
-        "traslade": "escoger"
-    },
-    {
-        "verb": "cost",
-        "past": "cost",
-        "participle": "cost",
-        "traslade": "costar"
-    },
-    {
-        "verb": "dive",
-        "past": "dove",
-        "participle": "diven",
-        "traslade": "bucear"
-    },
-    {
-        "verb": "do",
-        "past": "did",
-        "participle": "done",
-        "traslade": "hacer"
-    },
-    {
-        "verb": "draw",
-        "past": "drew",
-        "participle": "drawn",
-        "traslade": "dibujar"
-    },
-    {
-        "verb": "drink",
-        "past": "drank",
-        "participle": "drunk",
-        "traslade": "beber"
-    },
-    {
-        "verb": "drive",
-        "past": "drove",
-        "participle": "driven",
-        "traslade": "manejar"
-    },
-    {
-        "verb": "eat",
-        "past": "ate",
-        "participle": "eaten",
-        "traslade": "comer"
-    },
-    {
-        "verb": "fall",
-        "past": "fell",
-        "participle": "fallen",
-        "traslade": "caer"
-    },
-    {
-        "verb": "feed",
-        "past": "fed",
-        "participle": "fed",
-        "traslade": "alimentar"
-    },
-    {
-        "verb": "feel",
-        "past": "felt",
-        "participle": "felt",
-        "traslade": "sentir"
-    },
-    {
-        "verb": "find",
-        "past": "found",
-        "participle": "found",
-        "traslade": "encontrar"
-    },
-    {
-        "verb": "fly",
-        "past": "flew",
-        "participle": "flown",
-        "traslade": "volar"
-    },
-    {
-        "verb": "forget",
-        "past": "forgot",
-        "participle": "forgotten",
-        "traslade": "olvidar"
-    },
-    {
-        "verb": "forgive",
-        "past": "forgave",
-        "participle": "forgiven",
-        "traslade": "perdonar"
-    },
-    {
-        "verb": "freez",
-        "past": "froze",
-        "participle": "frozen",
-        "traslade": "congelar"
-    },
-    {
-        "verb": "get",
-        "past": "got",
-        "participle": "gotten",
-        "traslade": "conseguir"
-    },
-    {
-        "verb": "give",
-        "past": "gave",
-        "participle": "given",
-        "traslade": "dar"
-    },
-    {
-        "verb": "go",
-        "past": "went",
-        "participle": "gone",
-        "traslade": "ir"
-    },
-    {
-        "verb": "grow",
-        "past": "grew",
-        "participle": "grown",
-        "traslade": "cultivar"
-    },
-    {
-        "verb": "hear",
-        "past": "heard",
-        "participle": "heard",
-        "traslade": "oir"
-    },
-    {
-        "verb": "hide",
-        "past": "hid",
-        "participle": "hidden",
-        "traslade": "esconder"
-    },
-    {
-        "verb": "hit",
-        "past": "hit",
-        "participle": "hit",
-        "traslade": "golpear"
-    },
-    {
-        "verb": "hold",
-        "past": "held",
-        "participle": "held",
-        "traslade": "sostener / mantener"
-    },
-    {
-        "verb": "hurt",
-        "past": "hurt",
-        "participle": "hurt",
-        "traslade": "lastimar"
-    },
-    {
-        "verb": "keep",
-        "past": "kept",
-        "participle": "kept",
-        "traslade": "conservar / guardar"
-    },
-    {
-        "verb": "know",
-        "past": "knew",
-        "participle": "known",
-        "traslade": "saber"
-    },
-    {
-        "verb": "lead",
-        "past": "led",
-        "participle": "led",
-        "traslade": "mandar / guiar"
-    },
-    {
-        "verb": "leave",
-        "past": "left",
-        "participle": "left",
-        "traslade": "salir / dejar"
-    },
-    {
-        "verb": "lent",
-        "past": "lent",
-        "participle": "lent",
-        "traslade": "prestar"
-    },
-    {
-        "verb": "let",
-        "past": "let",
-        "participle": "let",
-        "traslade": "permitir"
-    },
-    {
-        "verb": "lose",
-        "past": "lost",
-        "participle": "lost",
-        "traslade": "perder"
-    },
-    {
-        "verb": "make",
-        "past": "made",
-        "participle": "made",
-        "traslade": "hacer"
-    },
-    {
-        "verb": "mean",
-        "past": "meant",
-        "participle": "meant",
-        "traslade": "significar"
-    },
-    {
-        "verb": "meet",
-        "past": "met",
-        "participle": "met",
-        "traslade": "conocer / reunirse / encontrarse"
-    },
-    {
-        "verb": "pay",
-        "past": "paid",
-        "participle": "paid",
-        "traslade": "pagar"
-    },
-    {
-        "verb": "read",
-        "past": "read",
-        "participle": "read",
-        "traslade": "leer"
-    },
-    {
-        "verb": "ride",
-        "past": "rode",
-        "participle": "ridden",
-        "traslade": "montar"
-    },
-    {
-        "verb": "ring",
-        "past": "rang",
-        "participle": "rung",
-        "traslade": "sonar"
-    },
-    {
-        "verb": "rise",
-        "past": "rose",
-        "participle": "risen",
-        "traslade": "subir / aumentar"
-    },
-    {
-        "verb": "run",
-        "past": "ran",
-        "participle": "run",
-        "traslade": "correr"
-    },
-    {
-        "verb": "say",
-        "past": "said",
-        "participle": "said",
-        "traslade": "decir"
-    },
-    {
-        "verb": "see",
-        "past": "saw",
-        "participle": "seen",
-        "traslade": "ver"
-    },
-    {
-        "verb": "sell",
-        "past": "sold",
-        "participle": "sold",
-        "traslade": "vender"
-    },
-    {
-        "verb": "send",
-        "past": "sent",
-        "participle": "sent",
-        "traslade": "enviar"
-    },
-    {
-        "verb": "set",
-        "past": "set",
-        "participle": "set",
-        "traslade": "establecer"
-    },
-    {
-        "verb": "shake",
-        "past": "shook",
-        "participle": "shaken",
-        "traslade": "sacudir"
-    },
-    {
-        "verb": "shoot",
-        "past": "shot",
-        "participle": "shot",
-        "traslade": "disparar"
-    },
-    {
-        "verb": "show",
-        "past": "showed",
-        "participle": "shown",
-        "traslade": "mostrar"
-    },
-    {
-        "verb": "sing",
-        "past": "sang",
-        "participle": "sung",
-        "traslade": "cantar"
-    },
-    {
-        "verb": "sink",
-        "past": "sank",
-        "participle": "sunk",
-        "traslade": "hundir"
-    },
-    {
-        "verb": "sit",
-        "past": "sat",
-        "participle": "sat",
-        "traslade": "sentarse"
-    },
-    {
-        "verb": "sleep",
-        "past": "slept",
-        "participle": "slept",
-        "traslade": "dormir"
-    },
-    {
-        "verb": "slide",
-        "past": "slid",
-        "participle": "slid",
-        "traslade": "resbalar / deslizar"
-    },
-    {
-        "verb": "speak",
-        "past": "spoke",
-        "participle": "spoken",
-        "traslade": "hablar"
-    },
-    {
-        "verb": "spend",
-        "past": "spent",
-        "participle": "spent",
-        "traslade": "gastar"
-    },
-    {
-        "verb": "spit",
-        "past": "spit",
-        "participle": "spit",
-        "traslade": "escupir"
-    },
-    {
-        "verb": "spread",
-        "past": "spread",
-        "participle": "spread",
-        "traslade": "extender"
-    },
-    {
-        "verb": "stand",
-        "past": "stood",
-        "participle": "stood",
-        "traslade": "pararse"
-    },
-    {
-        "verb": "steal",
-        "past": "stole",
-        "participle": "stolen",
-        "traslade": "robar"
-    },
-    {
-        "verb": "stick",
-        "past": "stuck",
-        "participle": "stuck",
-        "traslade": "clavar"
-    },
-    {
-        "verb": "sting",
-        "past": "stung",
-        "participle": "stung",
-        "traslade": "picar (picadura)"
-    },
-    {
-        "verb": "sweep",
-        "past": "swept",
-        "participle": "swept",
-        "traslade": "barrer"
-    },
-    {
-        "verb": "swim",
-        "past": "swam",
-        "participle": "swum",
-        "traslade": "nadar"
-    },
-    {
-        "verb": "take",
-        "past": "took",
-        "participle": "taken",
-        "traslade": "tomar (agarrar)"
-    },
-    {
-        "verb": "teach",
-        "past": "taught",
-        "participle": "taught",
-        "traslade": "enseñar"
-    },
-    {
-        "verb": "tear",
-        "past": "tore",
-        "participle": "torn",
-        "traslade": "rasgar"
-    },
-    {
-        "verb": "tell",
-        "past": "told",
-        "participle": "told",
-        "traslade": "decir"
-    },
-    {
-        "verb": "think",
-        "past": "thought",
-        "participle": "thought",
-        "traslade": "pensar / creer"
-    },
-    {
-        "verb": "throw",
-        "past": "threw",
-        "participle": "throw",
-        "traslade": "tirar"
-    },
-    {
-        "verb": "understand",
-        "past": "understood",
-        "participle": "understood",
-        "traslade": "entender"
-    },
-    {
-        "verb": "wake",
-        "past": "woke",
-        "participle": "woken",
-        "traslade": "despertar"
-    },
-    {
-        "verb": "wear",
-        "past": "wore",
-        "participle": "worn",
-        "traslade": "usar / vestir"
-    },
-    {
-        "verb": "win",
-        "past": "won",
-        "participle": "won",
-        "traslade": "ganar"
-    },
-    {
-        "verb": "write",
-        "past": "wrote",
-        "participle": "written",
-        "traslade": "excribir"
-    },
-    {
-        "verb": "seek",
-        "past": "sought",
-        "participle": "sought",
-        "traslade": "buscar"
-    }
-];
-
-let result;
+let verb;
 let counter = 0;
 let counterEnd = 5;
 const btn = document.getElementById('btnValidate');
@@ -563,51 +8,62 @@ const verbParticiple = document.getElementById('verbParticiple');
 const verbTraslade = document.getElementById('verbTraslade');
 document.querySelector('input').focus();
 
-function getVerb(list) {
-    let position = Math.random();
-    let op = Math.random();
+function getVerb() {
+    
+    fetch('./db/irregularVerbs.json')
+    .then((response) => response.json())
+    .then((json) => {
 
-    position = position * list.length;
-    op = op * 3;
+        let position = Math.random();
+        let op = Math.random();
+        
+        position = position * json.length;
+        op = op * 3;
 
-    switch (Math.floor(op)) {
-        case 0:
-            const present = list[Math.floor(position)];
+        switch (Math.floor(op)) {
+            case 0:
+                const present = json[Math.floor(position)];
+    
+                verbPresent.innerHTML = 'x';
+                verbPast.innerHTML = present.past;
+                verbParticiple.innerHTML = present.participle;
+                verbTraslade.innerHTML = present.traslade;
+    
+                verb = present.verb;
+                break;
+    
+            case 1:
+                const past = json[Math.floor(position)];
+    
+                verbPresent.innerHTML = past.verb;
+                verbPast.innerHTML = 'X';
+                verbParticiple.innerHTML = past.participle;
+                verbTraslade.innerHTML = past.traslade;
+    
+                verb = past.past;
+                break;
+                    
+            case 2: 
+                const participle = json[Math.floor(position)];
+    
+                verbPresent.innerHTML = participle.verb;
+                verbPast.innerHTML = participle.past;
+                verbParticiple.innerHTML = 'X';
+                verbTraslade.innerHTML = participle.traslade;
+    
+                verb = participle.participle;
+                break;
+    
+            default:
+                console.log('error');
+                break;
+        }
 
-            verbPresent.innerHTML = 'x';
-            verbPast.innerHTML = present.past;
-            verbParticiple.innerHTML = present.participle;
-            verbTraslade.innerHTML = present.traslade;
-
-            result = present.verb;
-            break;
-
-        case 1:
-            const past = list[Math.floor(position)];
-
-            verbPresent.innerHTML = past.verb;
-            verbPast.innerHTML = 'X';
-            verbParticiple.innerHTML = past.participle;
-            verbTraslade.innerHTML = past.traslade;
-
-            result = past.past;
-            break;
-                
-        case 2: 
-            const participle = list[Math.floor(position)];
-
-            verbPresent.innerHTML = participle.verb;
-            verbPast.innerHTML = participle.past;
-            verbParticiple.innerHTML = 'X';
-            verbTraslade.innerHTML = participle.traslade;
-
-            result = participle.participle;
-            break;
-
-        default:
-            console.log('error');
-            break;
-    }
+    })
+    .catch(error => {
+        console.error(error);
+    });
+    
 }
 
 function resetGame() {
@@ -646,7 +102,7 @@ function validate(input, verb) {
         document.getElementById('contador').style = 'color: #27AE60';
         document.querySelector('input').value='';
         document.querySelector('input').focus();
-        getVerb(list);
+        getVerb();
         document.getElementById('show').innerHTML = '-'
         document.getElementById('show').style = 'color: white';
         //showVerb(null);
@@ -667,9 +123,9 @@ function validate(input, verb) {
  
 /*********************************/
 
-getVerb(list);
+getVerb();
 
 btn.addEventListener('click', () => {
     const input = document.querySelector('input').value;
-    validate(input, result);
+    validate(input, verb);
 });
